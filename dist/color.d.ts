576f55747464fcb45f85cbf968dbb71ba07af84c
Chantal Jenkins <60161241+cjenkin1@users.noreply.github.com>
2023-12-08 00:17:13 -0500

    initial screens done-ish??

---
+++ dist/color.d.ts
@@ -0,0 +1,8 @@
+export declare class color {
+    red: number;
+    green: number;
+    blue: number;
+    hex: string;
+    constructor(r: number, g: number, b: number);
+}
+//# sourceMappingURL=color.d.ts.map
