2d33ce85e1a7fcf50285b50debd7c61f3f4685c9
Chantal Jenkins <60161241+cjenkin1@users.noreply.github.com>
2023-12-08 00:28:25 -0500

    Revert "initial screens done-ish??"
    This reverts commit ce7be689d05a19353ccf04969298e4461ec083f4.

--- dist/color.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class color {
-    red: number;
-    green: number;
-    blue: number;
-    hex: string;
-    constructor(r: number, g: number, b: number);
-}
-//# sourceMappingURL=color.d.ts.map
