81b570734d7edf9184348d1e7437f59245dad931
Chantal Jenkins <60161241+cjenkin1@users.noreply.github.com>
2023-12-16 19:12:49 -0500

    initial pages written

--- src/colors.ts
+++ src/colors.ts
@@ -11,3 +11,24 @@ export class color {
         this.hex = `#${(r > 15) ? r.toString(16) : '0'+r.toString(16)}${(g > 15) ? g.toString(16) : '0'+g.toString(16)}${(b > 15) ? b.toString(16) : '0'+b.toString(16)}`
     }
 }
+
+export function toHSL(): number[] {
+    let sol: number[] = Array.from({length: 3})
+    let rp = this.red/255
+    let gp = this.green/255
+    let bp = this.blue/255
+    let cmax = Math.max(rp, gp, bp)
+    let cmin = Math.min(rp, gp, bp)
+    let delta = cmax - cmin
+    sol[2] = (cmax + cmin)/2
+    sol[1] = (delta === 0) ? 0 : (delta)/(1-Math.abs(2*sol[2]-1))
+    if (delta === 0) sol[0] = 0
+    else if (cmax === rp) {
+        sol[0] = 60 * (((gp-bp)/delta) % 6)
+    } else if (cmax === gp) {
+        sol[0] = 60 * (((bp-rp)/delta) + 2)
+    } else {
+        sol[0] = 60 * (((rp-gp)/delta) + 4)
+    }
+    return sol
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,2 +1,49 @@
 import React from 'react'
-import * as ReactDom from 'react-dom/client'
+import * as ReactDOM from 'react-dom/client'
+
+import * as Color from 'src/colors'
+
+ReactDOM.createRoot(document.querySelector('#root')!).render(<App/>)
+
+function App() {
+    const [color, setColor] = React.useState<Color.color>()
+    return (
+        <>
+            <h1>colors.</h1>
+            {(!color) ? <IndexComp fn={setColor}/> : <ResultComp c={color}/>}
+        </>
+    )
+}
+
+function ColorForm(props: {fn: React.Dispatch<React.SetStateAction<Color.color|undefined>>}) {
+    function makeColor(e): void {
+        e.preventDefault()
+        let e0 = e.currentTarget as HTMLFormElement
+        props.fn(new Color.color(e0.elements['rIn'].value, e0.elements['gIn'].value, e0.elements['bIn'].value))
+    }
+    return (
+        <form onSubmit={makeColor}>
+            <fieldset>
+                <input type='number' min={0} max={255} name='rIn' id='rIn' defaultValue={0}/><br/>
+                <label htmlFor="rIn">red.</label><br/><br/>
+                <input type='number' min={0} max={255} name='gIn' id='gIn' defaultValue={0}/><br/>
+                <label htmlFor="gIn">green.</label><br/><br/>
+                <input type='number' min={0} max={255} name='bIn' id='bIn' defaultValue={0}/><br/>
+                <label htmlFor="bIn">blue.</label><br/><br/>
+                <button type="submit">submit.</button><button type="reset">reset.</button>
+            </fieldset>
+        </form>
+    )
+}
+
+function IndexComp(props: {fn: React.Dispatch<React.SetStateAction<Color.color|undefined>>}) {
+    return <ColorForm fn={props.fn}/>
+}
+
+function ResultComp(props: {c: Color.color}) {
+    return (
+        <>
+            <h3>your color is: {props.c.hex}</h3>
+        </>
+    )
+}
